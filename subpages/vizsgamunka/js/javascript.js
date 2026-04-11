8f892bac64bbc4a4325dff64cddb697431af2d57
szemi95 <sziemju@gmail.com>
2026-04-11 17:10:02 +0200

    2026.04.11.

--- subpages/vizsgamunka/js/javascript.js
+++ subpages/vizsgamunka/js/javascript.js
@@ -1,17 +1,23 @@
-let buttons = document.querySelectorAll(".kosarba");
-            buttons.forEach(button => {
-                button.addEventListener("click", function(){
-                    alert("Termék a kosárba került!");
-                });
-            });
-
-
 document.addEventListener("DOMContentLoaded", () => {
+
+  // KOSÁRBA GOMBOK
+  let buttons = document.querySelectorAll(".kosarba");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault(); // mobil + link esetén is biztonságos
+      alert("Termék a kosárba került!");
+    });
+  });
+
+  // KERESŐ FORM
   const form = document.getElementById("search-form");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Sajnáljuk, a kereső átmenetileg nem működik");
+    });
+  }
 
-    alert("Sajnáljuk, a kereső átmenetileg nem működik");
-  });
 });
